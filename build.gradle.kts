3310903d8d2532162bc2c9b8e3e2c6079791a7dd
ehsan <e@ehsanmsz.com>
2021-06-20 18:16:15 +0430

    Bump kotlin to 1.5.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("GradleDependency")
 buildscript {
     val compose_version by extra("1.0.0-beta07")
+
     repositories {
         google()
         mavenCentral()
     }
+
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0-beta03")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
